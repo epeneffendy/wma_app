f93ebda383fcbb526738d5cea577edaf2262291e
epeneffendy <epen.effendy@gmail.com>
2025-07-09 06:46:09 +0700

    perbaikan wma

---
+++ resources/views/admin/weighted_moving_average/index_new.blade.php
@@ -0,0 +1,146 @@
+@extends('layouts.admin.master')
+
+@section('title')
+    Dashboard
+@endsection
+
+@section('content')
+    <div class="row gy-4">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+                    <h5 class="card-title m-0 me-2">Weighted Moving Average</h5>
+                </div>
+
+                <div class="card-body">
+                    <div class="row mb-3">
+                        <label class="col-sm-2 col-form-label" for="date">Date</label>
+                        <div class="col-sm-4">
+                            <input class="form-control" type="date" id="date_periode"
+                                   name="date_periode" value="{{date('d/m/Y')}}"/>
+                        </div>
+                    </div>
+
+                    <div class="row mb-3">
+                        <label class="col-sm-2 col-form-label" for="total_days">Total Days</label>
+                        <div class="col-sm-2">
+                            <input class="form-control" type="number" id="total_days" name="total_days"/>
+                        </div>
+                    </div>
+
+                    <div class="mt-4">
+                        <button type="button" class="btn btn-primary me-2" id="fetch_wma"><span
+                                class="tf-icons mdi mdi-magnify me-1"></span>Calculate WMA
+                        </button>
+                    </div>
+
+                </div>
+            </div>
+            <br>
+            <div id="show_calculate_wma"></div>
+        </div>
+    </div>
+@endsection
+@section('scripts')
+    <script>
+        const base_prefix = "/administrator/wma";
+
+        $(document).ready(function () {
+            console.log("ini halaman manajemen stok");
+
+            $('#fetch_wma').click(function () {
+                validationCalculateWma();
+            });
+
+            $(document).on("click", "#count_wma", function () {
+                countWma();
+            });
+        });
+
+        function countWma() {
+            var $form = $("#show_data_actual");
+            var data = getFormData($form);
+            $.ajax({
+                type: 'POST',
+                url: base_prefix + '/weighted_moving_average/count_wma',
+                data: {
+                    "_token": "{{ csrf_token() }}",
+                    'form': data,
+                    'count': $('#count').val(),
+                    'periode': $('#periode').val(),
+                    'year': $('#year').val(),
+                    'total_month': $('#total_month').val()
+                }
+            })
+            redirect();
+        }
+
+        function redirect() {
+            window.location.href = "{{ route('admin.wma.weighted_moving_average.list')}}";
+
+        }
+
+        function validationCalculateWma() {
+            let success = true;
+            let message = 'validation success';
+
+            let date_periode = $('#date_periode').val();
+            let total_days = $('#total_days').val();
+            let year = $('#year').val();
+
+            if (date_periode == 0) {
+                success = false;
+                message = 'Date cannot be empty!'
+            }
+
+            if (total_days == '') {
+                success = false;
+                message = 'Total Days cannot be empty!'
+            }
+
+            if (total_days != '' && total_days >= 8) {
+                success = false;
+                message = 'Total Days should not exceed 7!'
+            }
+
+            if (success) {
+                let params = {
+                    'date_periode': date_periode,
+                    'total_days': total_days,
+                }
+                calculateWma(params)
+            } else {
+                swal('warning', message, 'warning');
+            }
+        }
+
+        function calculateWma(params) {
+            $.post(
+                base_prefix + '/weighted_moving_average/calculate_wma_new',
+                {
+                    "_token": "{{ csrf_token() }}",
+                    'date_periode': params.date_periode,
+                    'total_days': params.total_days,
+                },
+                function (data) {
+                    $('#show_calculate_wma').html('');
+                    $('#show_calculate_wma').html(data)
+
+                }
+            );
+        }
+
+
+        function getFormData($form) {
+            var unindexed_array = $form.serializeArray();
+            var indexed_array = {};
+
+            $.map(unindexed_array, function (n, i) {
+                indexed_array[n['name']] = n['value'];
+            });
+
+            return indexed_array;
+        }
+
+    </script>
+@endsection
---
+++ resources/views/admin/weighted_moving_average/partial/show_calculate_wma_new.blade.php
@@ -0,0 +1,44 @@
+<div class="row">
+    <div class="col-md-12">
+        <div class="card">
+            <div class="card-header">
+                <h5 class="card-title m-0 me-2">Calculate Weighted Moving Average</h5>
+            </div>
+
+            <div class="card-body">
+{{--                <input class="form-control" type="hidden" value="{{$count}}" id="count"--}}
+{{--                       name="count" readonly/>--}}
+                <form id="show_data_actual">
+
+                    @foreach($actual as $ind => $item)
+                        <div class="row mb-3">
+                            <div class="col-sm-3">
+                                <label class="col-sm-12 col-form-label" for="date">Date</label>
+                                <input class="form-control" type="text" value="{{$item['date']}}" id="date"
+                                       name="date_{{$ind}}" readonly/>
+                            </div>
+
+                            <div class="col-sm-3">
+                                <label class="col-sm-12 col-form-label" for="total_month">Actual</label>
+                                <input class="form-control" type="number" value="{{$item['actual']}}" id="actual"
+                                       name="actual_{{$ind}}" readonly/>
+                            </div>
+
+{{--                            <div class="col-sm-3">--}}
+{{--                                <label class="col-sm-12 col-form-label" for="total_month">Weight</label>--}}
+{{--                                <input class="form-control" type="number" id="weight" name="weight_{{$ind}}"/>--}}
+{{--                            </div>--}}
+                        </div>
+                    @endforeach
+                    <div class="mt-4">
+                        <button type="button" class="btn btn-primary me-2" id="count_wma"><span
+                                class="tf-icons mdi mdi-magnify me-1"></span>Proses WMA
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+
